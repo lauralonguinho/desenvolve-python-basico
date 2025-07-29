f6c0682921c5ed02d1b9ceda57badc3e1ad10342
lauralonguinho <laura.gontijo@pdbomdespacho.com.br>
2025-07-28 21:21:52 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,14 @@
+# Armazena os valores nas variáveis
+a = 5
+b = 2
+
+# Imprime o tipo de dados das variáveis a e b
+print(type(a))
+print(type(b))
+
+# Divide a variável a pela variável b e armazena o resultado em c
+c = a / b
+
+# Imprime o valor de c e seu tipo
+print(c)
+print(type(c))
